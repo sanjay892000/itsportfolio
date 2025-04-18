98d5f7ee82e3e02c68f344b9ce7cbd1a09a1f6a1
sanjay892000 <sanjay892000@gmail.com>
2025-04-18 15:57:57 +0530

    change

--- backend/passport.js
+++ backend/passport.js
@@ -20,7 +20,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://itsportfolio.onrender.com/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       const user = {
